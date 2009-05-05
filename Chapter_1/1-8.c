7b6f896b33676295891c55f0a757f5d5e775e5d9
LFTSQNCE <lftsqnce@kevin-funderburks-macbook-2.local>
2009-05-05 01:27:18 -0600

    1-8.c is messed up, it's giving wrong amounts for tabs and verticle tabs. I am guess blank spaces is the same as the verticle tabs?

---
+++ Chapter_1/1-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Blank, Tab and Newline Counter, Exercise 1-8
+main()
+{
+  int c, nl, tab, blank;
+
+  nl = 0;
+  while ((c = getchar()) != EOF)
+    if (c == '\n')
+      ++nl;
+    if (c == '\t')
+      ++tab;
+    if (c == '\v')
+      ++blank;
+  printf("%d new lines, %d tabs, %d blank lines.\n", nl, tab, blank);
+}
