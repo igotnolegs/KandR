672dd60cddbe225da6cd1ef6021c4fedfa5e1859
U-EXCHANGE07\kevinf <kevinf@kevinf.exchange07.socen.com>
2009-05-28 21:42:30 -0600

    1-13 completed

--- Chapter_1/1-13.c
+++ Chapter_1/1-13.c
@@ -4,19 +4,27 @@
 /*print a histogram of the length of words in the program's input*/
 main()
 {
-  int c, i, nletter;
+  int c, nletter, gbar;
   int nword[nletter];
   
-  i = nletter = 0;
+  nletter = 0;
   while ((c = getchar()) != EOF)
       if (c == ' ' || c == '\t' || c == '\n') {
         ++nword[nletter-1];
         nletter = 0;
-        }
+      }
       else
         nletter ++;
   
-  printf("digits =");
-  for (nletter = 0; nletter < 10; ++nletter)
-     printf(" %d", nword[nletter]);
+  printf("Graph of word length\n");
+  gbar = 0;
+  for (nletter = 0; nletter < 10; ++nletter) {
+      printf("%d:", nletter);
+      while (gbar != nword[nletter]) {
+      printf("=");
+      ++gbar;
+    }
+    printf("\n");
+    gbar = 0;
+  }
 }
